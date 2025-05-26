0568ce3094de13fcb893993ca5ba506caaacab3e
LikithaReddy1610 <likitha10.c@gmail.com>
2025-05-26 07:04:02 -0500

    Changes done based on review

--- src/features/contact/ContactPage.jsx
+++ src/features/contact/ContactPage.jsx
@@ -3,22 +3,22 @@ import withLayout from "../../layouts/HOC/withLayout";
 function ContactPage() {
   return (
     <div className="container">
-      <div className="contact-form">
-        <h2>Please fill the form</h2>
-        <form>
-          <div className="form-group">
-            <input type="text" placeholder="First Name" />
-            <input type="text" placeholder="Last Name" />
-          </div>
-          <div className="form-group">
-            <input type="email" placeholder="Enter your Email" />
-            <input type="text" placeholder="Phone" />
-          </div>
-          <div className="form-group">
-            <textarea placeholder="About Your Project"></textarea>
-          </div>
-          <button type="submit">SUBMIT</button>
-        </form>
+    <div className="contact-form">
+    <h2>Please fill the form</h2>
+    <form>
+      <div className="form-group">
+        <input type="text" placeholder="First Name" />
+        <input type="text" placeholder="Last Name" />
+      </div>
+      <div className="form-group">
+        <input type="email" placeholder="Enter your Email" />
+        <input type="text" placeholder="Phone" />
+      </div>
+      <div className="form-group">
+        <textarea placeholder="About Your Project"></textarea>
+      </div>
+      <button type="submit">SUBMIT</button>
+    </form>
       </div>
       <div className="contact-info">
         <h2>Let's Connect!</h2>
