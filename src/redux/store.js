a31fb792d6d78cc469457b79c8daeca4c08e6171
ademiltonnunes <ademilton.marcelo1@gmail.com>
2025-06-10 16:02:49 -0700

    feat: set categories endpoints and create redux and hook for categories

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import productReducer from './slices/productSlice';
 import cartReducer from './slices/cartSlice';
+import categoryReducer from './slices/categorySlice';
 
 export const store = configureStore({
   reducer: {
     products: productReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    categories: categoryReducer
   },
   middleware: (getDefaultMiddleware) =>
     getDefaultMiddleware({
